0481081976406e0caf839242830308d702d95b92
vk196 <victorumeh196@gmail.com>
2023-02-09 05:37:24 -0800

    challnege 03 done

---
+++ challenge-03/index.js
@@ -0,0 +1,18 @@
+const dishes = [
+    {
+        name: 'egg plant',
+        vegetarian: true
+    }
+    {
+        name: 'spaghetti and meatballs',
+        vegetarian: false
+    }
+];
+
+const veg = [];
+function vegFilter(){
+   dishes.filter(dishes.vegetarian === true, ()=>{
+    veg = dishes.push()
+   }
+   )
+}
