8fcb6d83fc469dbb92e0eb215d603cb7ae044eb1
vk196 <victorumeh196@gmail.com>
2023-02-09 05:20:24 -0800

    challenge 02 done

---
+++ challenge-02/index.js
@@ -0,0 +1,11 @@
+const movies = {
+  title: "Nun",
+  director: "Doe",
+  genre: "SCI-FI",
+  release: 2004,
+  rating: 5,
+};
+
+function getMovieOverview(obj) {
+  // if(obj.title === 'Nun') return `obj.title ${}`
+}
